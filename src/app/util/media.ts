58ec44425cbc799d6ef268a4d7a8458c3f027aaf
Gavin Abu-Zahra <gavinabuzahra@justwhatever.net>
2025-05-06 18:48:09 -0400

    Fixed signed url cache validation using 5 minutes in the past, instead of the furture

--- src/app/util/media.ts
+++ src/app/util/media.ts
@@ -12,7 +12,7 @@ export const s3client = new S3({
   }
 })
 
-export type DBSCacheItem = {
+export interface DBSCacheItem {
   _id: ObjectId,
   key: string,
   expires: number, // unix time in seconds
@@ -23,16 +23,16 @@ export type DBSCacheItem = {
 export async function findSignedURL(key: string, isAttachment: boolean = true): Promise<string> {
   async function getURL() {
     const url = await getSignedUrl(s3client, new GetObjectCommand({Bucket: bucket, Key: key, ResponseContentDisposition: isAttachment ? "attachment" : undefined}));
-    await db.collection<WithoutId<DBSCacheItem>>("scache").insertOne({key: key, url, expires: (Date.now() / 1000) - (5 * 60), attachment: isAttachment});
+    await db.collection<WithoutId<DBSCacheItem>>("scache").insertOne({key: key, url, expires: (Date.now() / 1000) + (5 * 60), attachment: isAttachment});
     return url;
   }
   const db = (await connectToDatabase()).db;
   const url = await db.collection<DBSCacheItem>("scache").find({key: key, attachment: isAttachment || false}).toArray();
   if(url.length < 1) return await getURL();
-  for (const e1 of url.filter(e => (Date.now() / 1000) < e.expires)) {
-    await db.collection<DBSCacheItem>("scache").deleteOne({_id: e1._id, attachment: isAttachment || false});
-  }
-  const valids = url.filter(e => (Date.now() / 1000) < e.expires).sort((a, b) => a.expires - a.expires);
+  
+  for (const e1 of url.filter(e => (Date.now() / 1000) > e.expires)) await db.collection<DBSCacheItem>("scache").deleteOne({_id: e1._id, attachment: isAttachment || false});
+  
+  const valids = url.filter(e => (Date.now() / 1000) <= e.expires).sort((a, b) => a.expires - a.expires);
   if(valids.length < 1) return await getURL();
   return valids[0].url
 }
